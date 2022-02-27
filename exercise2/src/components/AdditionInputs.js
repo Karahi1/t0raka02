4b4a5f261879f465ae0a79e6d32ef1ee11694ca3
Karahi1 <t0raka02@students.oamk.fi>
2022-02-27 15:17:49 +0200

    Commit

--- exercise2/src/components/AdditionInputs.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AdditionInputs(){
-    return <div>
-        <div>Kappalemäärä <input type="number" /></div >
-        <div>Kuvaus <input type="type" /></div >
-        <button>Lisää</button>
-    </div>
-}
